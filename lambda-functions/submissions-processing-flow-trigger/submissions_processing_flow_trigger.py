ddbcdd96941e9b5857f91fbba0f842db87278834
Slava Markov <xz>
2020-06-23 17:41:48 -0400

    lambda trigger reworked

--- lambda-functions/submissions-processing-flow-trigger/submissions_processing_flow_trigger.py
+++ lambda-functions/submissions-processing-flow-trigger/submissions_processing_flow_trigger.py
@@ -21,58 +21,58 @@ def lambda_handler(event, context):
     print(event)
     print("------- END of EVENT -------")
 
-    dest_bucket, overnight_flow_step_function_arn, intraday_flow_step_function_arn, emr_cluster_id = get_params()
+    raw_zone_bucket, \
+    token_map_bucket, \
+    transformed_zone_bucket, \
+    overnight_flow_step_function_arn, \
+    intraday_flow_step_function_arn, \
+    emr_cluster_id = get_params()
 
-    path_to_dest_bucket = "s3://" + dest_bucket + "/"
+    timestamp = datetime.now().strftime("%m_%d_%Y__%H_%M_%S")
+    path_to_raw_zone_bucket = "s3://" + raw_zone_bucket + "/" + timestamp + "/"
+    path_to_transformed_zone_bucket = "s3://" + transformed_zone_bucket + "/"
+    path_to_token_map_bucket = "s3://" + token_map_bucket + "/" + timestamp + "/"
+
     for record in event['Records']:
         bucket = record['s3']['bucket']['name']
         key = record['s3']['object']['key']
         path_to_source_file = "s3://" + bucket + "/" + key
-        print(f"--- ClusterId = {emr_cluster_id} | PathToSourceFile = {path_to_source_file} "
-              f"| PathToDestBucket = {path_to_dest_bucket} ---")
 
         step_function_arn = overnight_flow_step_function_arn \
             if key.endswith(".csv") \
             else intraday_flow_step_function_arn
-        trigger_step_function(emr_cluster_id, step_function_arn, path_to_source_file, path_to_dest_bucket)
 
+        step_function_params = f'{{ "ClusterId": "{emr_cluster_id}", ' \
+                               f'"PathToSourceFile": "{path_to_source_file}", ' \
+                               f'"PathToRawZoneBucket": "{path_to_raw_zone_bucket}", ' \
+                               f'"PathToTransformedZoneBucket": "{path_to_transformed_zone_bucket}", ' \
+                               f'"PathToTokenMapBucket": "{path_to_token_map_bucket}" }}'
+        print(f"params: {step_function_params}")
 
-def trigger_step_function(emr_cluster_id, step_function_arn, path_to_source_file, path_to_dest_bucket):
-    response = stepFunctionsClient.start_execution(
-        stateMachineArn=step_function_arn,
-        name="submissions_flow_at__" + datetime.now().strftime("%m_%d_%Y__%H_%M_%S"),
-        input=f'{{'
-              f'"ClusterId": "{emr_cluster_id}", '
-              f'"PathToSourceFile": "{path_to_source_file}", '
-              f'"PathToDestBucket": "{path_to_dest_bucket}"}}'
-    )
+        response = stepFunctionsClient.start_execution(
+            stateMachineArn=step_function_arn,
+            name="submissions_flow_at__" + timestamp,
+            input=step_function_params
+        )
 
     print("--- step function response ---")
     print(response)
 
 
 def get_params():
-    if len(sys.argv) > 2:
-        dest_bucket = sys.argv[2]
-    else:
-        dest_bucket = os.environ["TRANSFORMED_ZONE_BUCKET"]
+    raw_zone_bucket = os.environ["RAW_ZONE_BUCKET"]
+    token_map_bucket = os.environ["TOKEN_MAP_BUCKET"]
+    transformed_zone_bucket = os.environ["TRANSFORMED_ZONE_BUCKET"]
+    overnight_flow_step_function_arn = os.environ["OVERNIGHT_FLOW_STEP_FUNCTION_ARN"]
+    intraday_flow_step_function_arn = os.environ["INTRADAY_FLOW_STEP_FUNCTION_ARN"]
+    emr_cluster_id = os.environ["EMR_CLUSTER_ID"]
 
-    if len(sys.argv) > 3:
-        overnight_flow_step_function_arn = sys.argv[3]
-    else:
-        overnight_flow_step_function_arn = os.environ["OVERNIGHT_FLOW_STEP_FUNCTION_ARN"]
-
-    if len(sys.argv) > 4:
-        intraday_flow_step_function_arn = sys.argv[4]
-    else:
-        intraday_flow_step_function_arn = os.environ["INTRADAY_FLOW_STEP_FUNCTION_ARN"]
-
-    if len(sys.argv) > 5:
-        emr_cluster_id = sys.argv[5]
-    else:
-        emr_cluster_id = os.environ["EMR_CLUSTER_ID"]
-
-    return dest_bucket, overnight_flow_step_function_arn, intraday_flow_step_function_arn, emr_cluster_id
+    return raw_zone_bucket, \
+           token_map_bucket, \
+           transformed_zone_bucket, \
+           overnight_flow_step_function_arn, \
+           intraday_flow_step_function_arn, \
+           emr_cluster_id
 
 
 if __name__ == '__main__':
