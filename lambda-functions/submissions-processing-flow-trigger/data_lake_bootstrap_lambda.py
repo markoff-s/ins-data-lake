4a3f7974cb344b8adc9a94850f0c79d95c8dbc7a
Slava Markov <xz>
2020-07-05 22:16:28 -0400

    Revert "Merge branch 'develop'"
    This reverts commit 8968ebccac72ecaacfd4558b5fb7ac9959498bbd.

--- lambda-functions/submissions-processing-flow-trigger/data_lake_bootstrap_lambda.py
+++ lambda-functions/submissions-processing-flow-trigger/data_lake_bootstrap_lambda.py
@@ -10,30 +10,21 @@ def handler(event, context):
     try:
         if event['RequestType'] == 'Delete':
             # if delete, clean up data folders
-            print('Cleaning up buckets...')
-            buckets_to_clean = ["${QuarantineZoneBucket}", "${RawZoneBucket}", "${TransformedZoneBucket}", "${ReportingZoneBucket}"]
+            buckets_to_clean = ["${RawZoneBucket}", "${TransformedZoneBucket}", "${ReportingZoneBucket}"]
             for bucket_to_clean in buckets_to_clean:
                 bucket = s3.Bucket(bucket_to_clean)
                 objects_to_delete = []
                 for obj in bucket.objects.filter():
                     objects_to_delete.append({'Key': obj.key})
 
-                try:
-                    # this method throws an exception if the bucket is empty
-                    bucket.delete_objects(
-                        Delete={'Objects': objects_to_delete})
-                except Exception as e:
-                    print('Exception on cleaning up bucket "{}" :[{}]'.format(
-                        bucket.name, e))
+                bucket.delete_objects(Delete={'Objects': objects_to_delete})
         else:
             # create folders for raw overnight and intraday submissions
-            print('Initializing buckets...')
-            buckets_to_init = ["${QuarantineZoneBucket}", "${RawZoneBucket}"]
+            raw_bucket_name = "${RawZoneBucket}"
             keys = ["overnight/submissions/", "intraday/submissions/"]
-            for bucket in buckets_to_init:
-                for key in keys:
-                    obj = s3.Object(bucket, key)
-                    obj.put()
+            for key in keys:
+                obj = s3.Object(raw_bucket_name, key)
+                obj.put()
 
         cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
     except Exception as e:
