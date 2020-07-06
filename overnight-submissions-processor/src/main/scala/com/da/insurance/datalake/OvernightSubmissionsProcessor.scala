8f3a61b6a82a4df3b621ef02dae1f4153060342e
Vyacheslav (Slava) Markov <markoff-s@users.noreply.github.com>
2020-07-05 22:07:21 -0400

    Feature/tokenizer integration (#1)
    * added QuarantineZoneBucket
    
    * tried to run cfn stack updated from cli
    
    * fix BootstrapLambdaFunction
    
    * fix BootstrapLambdaFunction
    
    * fix BootstrapLambdaFunction
    
    * fix BootstrapLambdaFunction
    
    * overnight step function fixed
    
    * step function path fix
    
    * lambda trigger reworked
    
    * bootstrap lambda buckets paths fixed
    
    * infer schema for overnight data
    
    * reports generator fixed with new zip fields
    
    * tokenizer fix
    
    * lambda trigger fix
    
    * added glue crawler and db for toke map
    
    * ZipTokenMapGlueTable added
    
    Co-authored-by: Slava Markov <xz>

--- overnight-submissions-processor/src/main/scala/com/da/insurance/datalake/OvernightSubmissionsProcessor.scala
+++ overnight-submissions-processor/src/main/scala/com/da/insurance/datalake/OvernightSubmissionsProcessor.scala
@@ -47,13 +47,14 @@ object OvernightSubmissionsProcessor {
 
       spark =  sessionBuilder.getOrCreate()
 
-      val schemaFromJson = loadSchema(pathToSchema)
+//      val schemaFromJson = loadSchema(pathToSchema)
 
       val sourceData = spark
         .read
         .format("csv")
         .option("header", "true")
-        .schema(schemaFromJson)
+        .option("inferSchema", "true")
+//        .schema(schemaFromJson)
         .load(pathToSource)
         .withColumn(hudiTablePrecombineKey, current_timestamp().cast("long"))
         .withColumn(hudiTablePartitionColumn, regexp_replace(to_date(col("submitted_on"), "yyyy-MM-dd"), "-", "/"))
@@ -61,6 +62,7 @@ object OvernightSubmissionsProcessor {
 //        .drop("submitted_on")
       //.cache()
 
+      // clean up col names in case there's something invalid
       val cleanedSourceData = normalizeColumnNames(sourceData)
 
       saveToHudi(cleanedSourceData, pathToDestination, isInitialRun)
@@ -73,7 +75,8 @@ object OvernightSubmissionsProcessor {
     }
   }
 
-  private def loadSchema(pathToSchema: String) = {
+  // schema is easily inferred after switch to normalized source data
+  /*private def loadSchema(pathToSchema: String) = {
     // somehow this is not loading on EMR... smth wrong with the path
     /*val url = ClassLoader.getSystemResource(pathToSchema)
     val schemaSource = Source.fromFile(url.getFile).getLines.mkString*/
@@ -81,7 +84,7 @@ object OvernightSubmissionsProcessor {
     val schemaSource = "{\"fields\":[{\"metadata\":{},\"name\":\"Submission_ID\",\"nullable\":false,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Submission_Status\",\"nullable\":false,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Submitted_On\",\"nullable\":false,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Underwriter\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Organization\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Street\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"City\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"State\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Zip\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Website\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Years_at_Location\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Principal_Name\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Years_in_Business\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Nature_of_Operation\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Previous_Carrier\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Expiry_Date\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Expiring_Premium\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Building_Owned\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Area_Occupied\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Stories\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Year_Built\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Age\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Class_of_Construction\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Wall\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Roof\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Floor\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Roof_Updated\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Wiring_Updated\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Plumbing_Updated\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Heating_Updated\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Occupied_Stories\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Adjacent_North\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Adjacent_South\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Adjacent_East\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Adjacent_West\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Fire_Protection\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Fire_Alarm\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Sprinklered\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Burglar_Alarm\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Deadbolt_Locks\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Other_Protection\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Safe\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Safe_Type\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Average_Cash\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Maximum_Cash\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Mortgagee\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Building_Value\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Equipment_Value\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Improvements_Value\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Office_Contents\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"EDP_Equipment\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"EDP_Data_Media\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Laptops_Projectors\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Customer_Goods\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Others_Property\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Stock_Value\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Gross_Earnings\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Profits\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Transit\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Employee_Dishonesty\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Money_Orders_Securities\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Flood\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Earthquake\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Boiler\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Number_of_Boilers\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Boiler_Maintenance_Contract\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Air_Conditioning\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Central_Air_Conditioning\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Tons\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Air_Conditioning_Maintenance_Contract\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Automatic_Backup_Power\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Voltage_Surge_Suppression\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Claim_Date\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Claim_Description\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Claim_Amount\",\"nullable\":true,\"type\":\"integer\"},{\"metadata\":{},\"name\":\"Signed_Name\",\"nullable\":true,\"type\":\"string\"},{\"metadata\":{},\"name\":\"Title_Position\",\"nullable\":true,\"type\":\"string\"}],\"type\":\"struct\"}"
 
     DataType.fromJson(schemaSource).asInstanceOf[StructType]
-  }
+  }*/
 
   private def normalizeColumnNames(sourceData: DataFrame) = {
     // prepare columns for parquet
