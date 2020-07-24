db6560bf17f2d9b95b4783051246efb15dadc9c3
Slava Markov <xz>
2020-07-24 09:39:36 -0400

    added spark-xml-bootstrap.sh

--- init-scripts/emr-hudi-bootstrap.sh
+++ init-scripts/emr-hudi-bootstrap.sh
@@ -1,6 +1,4 @@
 yum update -y && \
 hdfs dfs -mkdir -p /apps/hudi/lib && \
 hdfs dfs -copyFromLocal /usr/lib/hudi/hudi-spark-bundle.jar /apps/hudi/lib/hudi-spark-bundle.jar && \
-hdfs dfs -copyFromLocal /usr/lib/spark/external/lib/spark-avro.jar /apps/hudi/lib/spark-avro.jar && \
-cd /usr/lib/spark/jars && \
-sudo wget https://repo1.maven.org/maven2/com/databricks/spark-xml_2.11/0.9.0/spark-xml_2.11-0.9.0.jar
+hdfs dfs -copyFromLocal /usr/lib/spark/external/lib/spark-avro.jar /apps/hudi/lib/spark-avro.jar
---
+++ init-scripts/spark-xml-bootstrap.sh
@@ -0,0 +1,2 @@
+cd /usr/lib/spark/jars && \
+sudo wget https://repo1.maven.org/maven2/com/databricks/spark-xml_2.11/0.9.0/spark-xml_2.11-0.9.0.jar
